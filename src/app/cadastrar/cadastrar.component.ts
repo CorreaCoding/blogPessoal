58c955ce33cdf948675a1366a88358a0e7b5a050
Amanda Corrêa <84135805+CorreaCoding@users.noreply.github.com>
2021-08-30 12:08:08 -0300

    Consumo de API - Login e Cadastro

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,9 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUsuarios: string
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+  }
+
+  confirmeSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUsuarios = event.target.value
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoUsuarios
+
+    if (this.usuario.senha != this.confirmarSenha) {
+      alert('As senhas não são compatíveis')
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario)=>{
+        this.usuario = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
   }
 
 }
